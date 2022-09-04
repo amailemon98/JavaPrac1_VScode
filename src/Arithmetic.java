9bd6fd7fb68375debebfbe907381aae7833a45f5
Junhyung <amailemon98@naver.com>
2022-09-04 23:00:49 +0900

    arithmetic operation basic

---
+++ src/Arithmetic.java
@@ -0,0 +1,8 @@
+public class Arithmetic {
+    public static void main(String[] args) throws Exception {
+        System.out.println(1+2);
+        System.out.println(1.2+1.3);
+        System.out.println(2*5);
+        System.out.println(10/2);
+    }
+}
