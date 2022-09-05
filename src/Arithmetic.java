99bb093c20754097cdcb42a02cfacb3a1c6f0401
Junhyung <amailemon98@naver.com>
2022-09-05 22:59:06 +0900

    call from the other .java file

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,8 +1,15 @@
+
+
 public class Arithmetic {
     public static void main(String[] args) throws Exception {
         System.out.println(1+2);
         System.out.println(1.2+1.3);
         System.out.println(2*5);
         System.out.println(10/2);
+        System.out.println('일');
+        System.out.println("이삼사");
+        System.out.println("그는 \"사랑이라고 말했다\" ");
+        PackPackageTest.app();
+        
     }
 }
---
+++ src/PackPackageTest.java
@@ -0,0 +1,7 @@
+public class PackPackageTest{
+    public static void app(){
+        System.out.println('일');
+        System.out.println("이삼사");
+        System.out.println("그는 \"사랑이라고 말했다\" ");
+    }
+}
