dde2ce2b353ed0883a0f07432a1158b22dfa5acd
Junhyung <amailemon98@naver.com>
2022-09-08 23:31:44 +0900

    VariablesAndTypes byte to boolean comment added

--- src/VariablesAndTypes.java
+++ src/VariablesAndTypes.java
@@ -11,3 +11,11 @@ public class VariablesAndTypes {
         System.out.println(s3);
     }
 }
+//byte  =   number, 1 byte
+//short =   number, 2 bytes
+//int   =   number, 4 bytes
+//long  =   number, 8 bytes
+//float =   float number, 4 bytes
+//double=   float number, 8 bytes
+//char  =   a character, 2 bytes
+//boolean   =   true or false, 1 byte
