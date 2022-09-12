2c7840e6e58ef1feb96fca8e93faa951c5017f94
Junhyung <amailemon98@naver.com>
2022-09-12 23:05:09 +0900

    Conditionals.booleanOperators added.

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -14,4 +14,18 @@ public class Conditionals{
             System.out.println("Ohhh! S a is 4!");
         }
     }
+    public static void booleanOperators(){
+        int a = 4;
+        int b = 5;
+        boolean result;
+        result = a < b; //true
+        result = a > b; //false
+        result = a <= 4; // a smaller or equal to 4 - true
+        result = b >= 6; // a bigger or equal to 6 - false
+        result = a == b; // a equal to b - false
+        result = a != 6; // a is not equal to 6 - false
+        result = a > b || a < b; // Logical or - true
+        result = 3 < a && a < 6; // Logical and - true
+        result = !result; // Logical not - false
+    }
 }
