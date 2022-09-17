5991f6e73ae6c8dc7e683c7a1e57e1fb492be834
Junhyung <amailemon98@naver.com>
2022-09-17 19:41:15 +0900

    Loops.java Loops.class _For Method added.

---
+++ src/Loops.java
@@ -0,0 +1,21 @@
+public class Loops {
+    public static void _For(){
+        //The for loop has three sections.
+        //for(1. (int i = 0;) 2. (i < 3;) 3. (i++)){
+        //
+        //}
+        //int i = 0;
+        //i < 3     0 < 3 = true
+        //          Inside of loop
+        //i++       i is now 1
+        //i < 3     1 < 3 = true
+        //          Inside of loop
+        //i ++      i is now 2
+        //i < 3     2 < 3 = true
+        //          Inside of loop
+        //i++       i is now 3
+        //i < 3     3 < 3 = false
+        //          Loop is done...
+    }
+    
+}
