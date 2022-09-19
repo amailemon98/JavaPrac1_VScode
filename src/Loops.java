2d025613bf1d9302597a48fd37e7870971fb0a03
Junhyung <amailemon98@naver.com>
2022-09-19 23:21:51 +0900

    Loops.java Loops.class _Continue Method added.

--- src/Loops.java
+++ src/Loops.java
@@ -50,4 +50,18 @@ public class Loops {
         }
         System.out.println(i);
     }
+    public static void _Continue(){
+        int i;
+        for(i = 0; i < 5; i++){
+            if (i >= 3){
+                break;
+            }
+            System.out.println("Yuhu");
+            if (i >= 1){
+                continue;
+            }
+            System.out.println("Tata");
+        }
+        System.out.println(i);
+    }
 }
