a26321257980c979c3f7c8dce7ee780516229600
Junhyung <amailemon98@naver.com>
2022-09-20 17:02:22 +0900

    Functions.java Functions.class _Functions, bar Method added.

---
+++ src/Functions.java
@@ -0,0 +1,15 @@
+public class Functions {
+    public static void _Functions(){
+        //Do Something here
+    }
+
+    //Arguments start from below
+    public void bar(int num1, int num2){
+        int a = 3;
+        int b = 5;
+        bar(a,b);
+
+        //int num1 = a;
+        //int num2 = b;
+    }
+}
