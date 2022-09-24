e556b108b0627b40f6da12f4f5c565659b31179f
Junhyung <amailemon98@naver.com>
2022-09-24 22:24:08 +0900

    Objects.java Point.class, Point2.class added.

--- src/Objects.java
+++ src/Objects.java
@@ -7,3 +7,27 @@ public class Objects {
 
     Point p = new Point();
 }
+
+class Point{
+    int x;
+    int y;
+
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    
+}
+}
+
+class Point2{
+    int x;
+    int y;
+    
+    Point2(){
+        this(0,0);
+    }
+    Point2(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
