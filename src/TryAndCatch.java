e2396f99f3434a5991d92443c432ac4ec65c9aef
Junhyung <amailemon98@naver.com>
2022-09-29 23:12:38 +0900

    TryAndCatch.java added.

---
+++ src/TryAndCatch.java
@@ -0,0 +1,9 @@
+public class TryAndCatch {
+    try{
+        //code here
+    }catch(ExceptionHere name){
+        //Replace ExceptionHere with your exception and name with the name of your exception.
+        //code if exception "ExceptionHere" is thrown.
+    }
+    
+}
