2059df1d0c7d52b20c7f81db4a3690a791c69515
Junhyung <amailemon98@naver.com>
2022-09-30 15:20:36 +0900

    AbstractClassesVersusInterface.java added.

---
+++ src/AbstractClassesVersusInterface.java
@@ -0,0 +1,10 @@
+public class AbstractClassesVersusInterface {
+    abstract class X inplements Y{
+        //implements all but one method of Y
+    }
+
+    class XX extends X{
+        //implements the remaining method in Y
+    }
+    
+}
