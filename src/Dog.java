942b5112a70182ce8562f62bc92758a5031548a9
Junhyung <amailemon98@naver.com>
2022-10-01 19:35:24 +0900

    class Dog.java added.

---
+++ src/Dog.java
@@ -0,0 +1,12 @@
+public class Dog {
+    boolean feed(boolean timeToEat){
+        //pour food into bowl
+        return true;
+    }
+    void groom(){
+        //brush well
+    }
+    void pet(){
+        //pet cautiously
+    }
+}
