cb5ee205c2eb9c96379e2a7e77afa0e97a8d590c
Junhyung <amailemon98@naver.com>
2022-10-01 19:39:58 +0900

    Class Tiger.java added.

---
+++ src/Tiger.java
@@ -0,0 +1,13 @@
+public class Tiger implements Animal {
+    boolean feed(boolean timeToEat){
+        // toss raw meat into cage
+        return true;
+    }
+    void groom(){
+        // tranquilize
+        // brush well
+    }
+    void pet(){
+        // DO NOT PET
+    }
+}
