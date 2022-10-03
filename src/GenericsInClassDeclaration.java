8df89f0a9ff85b4d087b9c883fd25a557cfb2525
Junhyung <amailemon98@naver.com>
2022-10-03 13:37:16 +0900

    GenericsInClassDeclaration.java fixed.

--- src/GenericsInClassDeclaration.java
+++ src/GenericsInClassDeclaration.java
@@ -11,4 +11,10 @@ public class GenericsInClassDeclaration<Class1, Class2> {
     public Class2 getBob2(){
         return (this.bob2);
     }
+    public void testBob(){
+        YourClass<String, Integer> leBobs = new YourClass<>("Words",42); // in this case, <> is the same as putting <String, Integer>
+        String a = leBobs.getBob1();
+        int b = leBobs.getBob2();
+        System.out.println("bob1 is '" + a + "', and bob2 is '" + b + "'.");
+    }
 }
