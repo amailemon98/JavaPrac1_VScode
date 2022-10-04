d95580bba07e7058c369cc3a641497796c732502
Junhyung <amailemon98@naver.com>
2022-10-04 23:07:05 +0900

    GenericVariable.java added.

---
+++ src/GenericVariable.java
@@ -0,0 +1,8 @@
+public class GenericVariable {
+    class Main{
+        public static void main(String args[])
+        Generic<String> strvar = new Generic<String>("I love Java");// Here the object is created for Generic class of type String.
+        System.out.println(strvar.getVariable());
+    }
+    
+}
