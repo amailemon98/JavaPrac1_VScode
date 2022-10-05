91028a16c33eea18dc48de216dec30a6d7212a81
Junhyung <amailemon98@naver.com>
2022-10-05 23:12:59 +0900

    DataStructureAndAlgorithms.java added.

---
+++ src/DataStructureAndAlgorithms.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+import javax.swing.event.MenuKeyListener;
+
+public class DataStructureAndAlgorithms {
+    List<String> myList = new ArrayList<String>();
+    myList.add("james");
+    myList.add("Shane");
+    myList.add("Abby");
+    System.out.println(myList);
+}
