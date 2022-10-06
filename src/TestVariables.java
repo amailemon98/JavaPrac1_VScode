93309998271d31bb2dcdac3ecb2c10348ba65cc0
Junhyung <amailemon98@naver.com>
2022-10-06 23:05:32 +0900

    TestVariables.java added.

---
+++ src/TestVariables.java
@@ -0,0 +1,9 @@
+public class TestVariables {
+    int data = 20; // instance variable
+    static int number = 10; // static variable
+    void someMethod()
+    {
+        int num = 30; //local variable
+    }
+    
+}
