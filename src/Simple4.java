a292511c3fc48f4750af8d4b3f305b17b6f6e586
Junhyung <amailemon98@naver.com>
2022-10-13 23:05:28 +0900

    Simple4.java added.

---
+++ src/Simple4.java
@@ -0,0 +1,9 @@
+public class Simple4 {
+    public static void main(String[] args){
+        //Overflow
+        int a = 130;
+        byte b = (byte)a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
