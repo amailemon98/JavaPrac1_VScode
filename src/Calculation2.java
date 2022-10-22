2a725bcaf4682979eafa8d0248766dcc72cc7628
Junhyung <amailemon98@naver.com>
2022-10-22 21:59:06 +0900

    Calculation2.java added.

---
+++ src/Calculation2.java
@@ -0,0 +1,9 @@
+public class Calculation2 {
+    public static void main(STring[] args){
+        String[] names = {"Java","C","C++","Python","JavaScript"};
+        System.out.println("Printing the content of the array names:\n");
+        for(String name:names){
+            System.out.println(name);
+        }
+    }
+}
