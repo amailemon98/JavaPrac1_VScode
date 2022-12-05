005cded761d4456ad44cb9d9f2d7f95434e9ba7a
Junhyung <amailemon98@naver.com>
2022-12-05 22:23:57 +0900

    Calculate.java added.

---
+++ src/Calculate.java
@@ -0,0 +1,12 @@
+public class Calculate {
+    public int sum(int input1, int input2){
+        return input1 + input2;
+    }
+
+    public static void main(String[] args){
+        Calculate obj = new Calculate();
+        int result = obj.sum(40,20);
+
+        System.out.println("Addition of numbers:" + result);
+    }
+}
