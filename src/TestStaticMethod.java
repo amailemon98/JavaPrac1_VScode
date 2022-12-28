a3105f915dad9ace6b929c85ebbb2c6085930dc7
Junhyung <amailemon98@naver.com>
2022-12-28 22:26:18 +0900

    TestStaticMethod.java modified.

--- src/TestStaticMethod.java
+++ src/TestStaticMethod.java
@@ -6,7 +6,7 @@ class Student_M{
         college = "BBDIT";
     }
 
-    Student(int r, String n){
+    Student_M(int r, String n){
         rollno = r;
         name = n;
     }
@@ -18,9 +18,9 @@ public class TestStaticMethod {
     public static void main(String args[]){
         Student_M.change();
 
-        Student_M s1 = new Student_M();
-        Student_M s2 = new Student_M();
-        Student_M s3 = new Student_M();
+        Student_M s1 = new Student_M(111,"Karen");
+        Student_M s2 = new Student_M(222,"Aryan");
+        Student_M s3 = new Student_M(333,"Sonoo");
 
         s1.display();
         s2.display();
