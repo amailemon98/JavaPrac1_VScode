0256a4a439632d3b6d3d52fc2ed8ba5c40a268c9
Junhyung <amailemon98@naver.com>
2022-12-29 20:03:31 +0900

    Calculate_1.java added.

---
+++ src/Calculate_1.java
@@ -0,0 +1,10 @@
+class Calculate_1{
+    static int cube(int x){
+        return x*x*x;
+    }
+
+    public static void main(String args[]){
+        int result = Calculate_1.cube(5);
+        System.out.println(result);
+    }
+}
