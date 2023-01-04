911443418cfc5a3409bbde5ca28c0c7152788b55
Junhyung <amailemon98@naver.com>
2023-01-04 19:13:31 +0900

    TestThis3.java added.

---
+++ src/TestThis3.java
@@ -0,0 +1,20 @@
+class Student{
+    int rollno;
+    String name;
+    float fee;
+    Student(int r, String n, float f){
+        rollno=r;
+        name=n;
+        fee=f;
+    }
+    void display(){System.out.println(rollno+" "+name+" "+fee);}
+}
+
+public class TestThis3 {
+    public static void main(String args[]){
+        Student s1 = new Student(111, "ankit", 5000f);
+        Student s2 = new Student(112, "subit", 6000f);
+        s1.display();
+        s2.display();
+    }
+}
