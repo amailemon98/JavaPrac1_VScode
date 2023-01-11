fc2e437b2e4bf8eb7af1117eee2571af0f307c56
Junhyung <amailemon98@naver.com>
2023-01-11 18:57:37 +0900

    A4.java added.

---
+++ src/A4.java
@@ -0,0 +1,20 @@
+class B{
+    A4 obj;
+    B(A4 obj){
+        this.obj=obj;
+    }
+    void display(){
+        System.out.println(obj.data);
+    }
+}
+
+public class A4 {
+    int data = 10;;
+    A4(){
+        B b=new B(this);
+        bb.display();
+    }
+    public static void main(String args[]){
+        A4 a = new A4();
+    }
+}
